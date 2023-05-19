8b3dd078f8b588a6dbd87275b79d8b119c4a56da
AlGol <algol1@mail.ru>
2023-05-19 18:18:28 +0300

    lessons

--- Lesson4_hometasks/Task3.py
+++ Lesson4_hometasks/Task3.py
@@ -5,3 +5,9 @@ def amulet_area(a, b, c):
     return S
 
 assert amulet_area(3, 4, 5) == 6
+
+summ = 0
+a = [int(i) for i in input().split()]
+for i in a:
+    summ += i
+print(summ)
--- Lesson4_hometasks/Task5.py
+++ Lesson4_hometasks/Task5.py
@@ -4,8 +4,8 @@ my_array = np.random.random(100)
 my_array[my_array == np.max(my_array)] = 1
 my_array[my_array == np.min(my_array)] = 0
 
-#print(np.max(my_array), np.min(my_array))
-#print(my_array)
+print(np.max(my_array), np.min(my_array))
+print(my_array)
 
 
 my_array = np.random.randint(0,50,[5,6])## Your code here
